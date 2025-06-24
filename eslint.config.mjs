b9d55132aed357aacc577a750e61547876a11bd2
Marco A Perez S <maps1942gmail.com>
2025-06-24 00:31:16 -0600

    config lint final

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,13 @@
-// @ts-check
 import antfu from "@antfu/eslint-config";
 
-export default antfu({
+// @ts-check
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+// TODO: add tailwindcss plugin
+
+export default withNuxt(antfu({
   type: "app",
+  vue: true,
   typescript: true,
   formatters: true,
   stylistic: {
@@ -10,8 +15,17 @@ export default antfu({
     semi: true,
     quotes: "double",
   },
+  ignores: [".pnpm-store/**", "**/migrations/*"],
 }, {
   rules: {
+    "vue/max-attributes-per-line": ["error", {
+      singleline: {
+        max: 2,
+      },
+      multiline: {
+        max: 1,
+      },
+    }],
     "ts/no-redeclare": "off",
     "ts/consistent-type-definitions": ["error", "type"],
     "no-console": ["warn"],
@@ -26,4 +40,4 @@ export default antfu({
       ignore: ["README.md"],
     }],
   },
-});
+}));
