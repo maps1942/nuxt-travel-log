ec9707bc2202762e30db6d7a77a7af040107c73d
Marco Pérez <maps1942@gmail.com>
2025-06-25 22:48:15 -0600

    Setup tailwild & daisyUI & icons (#49)
    Co-authored-by: Marco A Perez S <maps1942œgmail.com>

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <button class="btn btn-soft btn-secondary">
+    View map <Icon name="tabler:map" size="32" />
+  </button>
 </template>
