377408edad1b0dc18148ab40ae2abd3167ddcfdf
Marco A Perez S <maps1942gmail.com>
2025-06-27 12:56:13 -0600

    :sparkles: add landing page

--- app.vue
+++ app.vue
@@ -1,8 +1,5 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  <button class="btn btn-soft btn-secondary">
-    View map <Icon name="tabler:map" size="32" />
-  </button>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="hero bg-base-200 container mx-auto mt-4">
+      <div class="hero-content text-center min-h-96">
+        <div class="max-w-md">
+          <h1 class="text-5xl font-bold">
+            Travel Log
+          </h1>
+          <p class="py-6">
+            Welcome to the Travel Log app! This is a simple application to log your travels and share them with others.
+            You can create an account, log in, and start adding your travel experiences.
+          </p>
+          <button class="btn btn-accent">
+            Sign in with Github
+            <Icon name="tabler:brand-github" size="24" />
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
