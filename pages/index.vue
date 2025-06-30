88b1971722ac101b1466e07f1fbcba12a736bf95
Marco A Perez S <maps1942gmail.com>
2025-06-30 13:36:21 -0600

    :sparkles: add theme toggle

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="hero bg-base-200 container mx-auto mt-4">
+    <div class="hero bg-base-300 container mx-auto mt-4">
       <div class="hero-content text-center min-h-96">
         <div class="max-w-md">
           <h1 class="text-5xl font-bold">
