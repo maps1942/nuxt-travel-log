5e8b3d9a67b0cd6295366ead1e444b082c357ae3
Marco A Perez S <maps1942gmail.com>
2025-07-01 16:00:56 -0600

    :sparkles: setup db

---
+++ lib/db/schema/location-log.ts
@@ -0,0 +1,17 @@
+import { int, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+import { location } from "./location";
+
+export const locationLog = sqliteTable("locationLog", {
+  id: int().primaryKey({ autoIncrement: true }),
+  name: text().notNull(),
+  description: text(),
+  startedAt: int().notNull(),
+  endedAt: int().notNull(),
+  lat: real().notNull(),
+  long: real().notNull(),
+  locationId: int().notNull().references(() => location.id, { onDelete: "cascade" }),
+  // userId: int().notNull().references(() => user.id, { onDelete: "cascade" }),
+  createdAt: int().notNull().$default(() => Date.now()),
+  updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
+});
