29a854abbcf9f3ed608e518736dcc9fff4f2d579
Marco A Perez S <maps1942gmail.com>
2025-07-02 13:27:11 -0600

    :sparkles: auth setup

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,7 +1,6 @@
 import { drizzle } from "drizzle-orm/libsql";
 
-import env from "~/lib/env";
-
+import env from "../env";
 import * as schema from "./schema";
 
 // You can specify any property from the libsql connection options
@@ -10,7 +9,7 @@ const db = drizzle({
     url: env.TURSO_DATABASE_URL,
     authToken: env.NODE_ENV === "development" ? undefined : env.TURSO_AUTH_TOKEN,
   },
-  casingg: "snake_case",
+  casing: "snake_case",
   schema,
 });
 export default db;
