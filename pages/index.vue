1f8bbf03db7892dcaee9ac13439bdb06d30ede77
Marco Pérez <maps1942@gmail.com>
2025-07-08 17:23:53 -0600

    :sparkles: add auth middleware (#59)

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+const authStore = useAuthStore();
+</script>
+
 <template>
   <div>
     <div class="hero bg-base-300 container mx-auto mt-4">
@@ -10,7 +14,14 @@
             Welcome to the Travel Log app! This is a simple application to log your travels and share them with others.
             You can create an account, log in, and start adding your travel experiences.
           </p>
-          <AuthButton />
+          <AuthButton v-if="!authStore.user" />
+          <NuxtLink
+            v-else
+            to="/dashboard"
+            class="btn btn-primary"
+          >
+            Start Logging
+          </NuxtLink>
         </div>
       </div>
     </div>
